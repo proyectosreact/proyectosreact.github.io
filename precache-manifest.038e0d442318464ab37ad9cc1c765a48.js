1671cd282c3b1fdd151402f7964d9033da6dafe2
jose luis zuniga flores <buda@Mac-mini-de-jose.local>
2020-06-07 15:47:36 -0500

    agregando todo

---
+++ precache-manifest.038e0d442318464ab37ad9cc1c765a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b86579d10997257cbd6dd4a799fd625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a52dca4bf6ffbfcb1724",
+    "url": "/static/css/main.b91d3704.chunk.css"
+  },
+  {
+    "revision": "16cbbc39e10bf9ffa700",
+    "url": "/static/js/2.5b30f740.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.5b30f740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52dca4bf6ffbfcb1724",
+    "url": "/static/js/main.77b3b508.chunk.js"
+  },
+  {
+    "revision": "531789f098ad02840335",
+    "url": "/static/js/runtime-main.15766f25.js"
+  },
+  {
+    "revision": "581e62d7955b848ec2d2028e670547cd",
+    "url": "/static/media/slide.581e62d7.png"
+  }
+]);
